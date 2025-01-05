e90c4ff0319f69556d99379b24d59bfe02650d80
3x3l0n <josuejaureguicamacho@gmail.com>
2025-01-04 19:22:19 -0500

    final crud commit

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,50 +1,26 @@
 import { Router } from 'express';
-import { pool } from '../db.js';
+import {
+  getUser,
+  getUsers,
+  createUser,
+  deleteUser,
+  updateUser,
+} from '../controllers/users.controllers.js';
 
 const router = Router();
 
-router.get('/users', async (req, res) => {
-  const { rows } = await pool.query('SELECT * FROM users');
-  res.json(rows);
-});
+router.get('/users', getUsers);
 
 //GET
-router.get('/users/:id', (req, res) => {
-  const { id } = req.params;
-  res.send('obteniendo usuario' + id);
-});
+router.get('/users/:id', getUser);
 
 //POST
-router.post('/users', async (req, res) => {
-  const data = req.body;
-  const { rows } = await pool.query(
-    'INSERT INTO users (name, email) VALUES ($1, $2) RETURNING *',
-    [data.name, data.email]
-  );
-
-  return res.json(rows[0]);
-});
+router.post('/users', createUser);
 
 //DELETE
-router.delete('/users/:id', async (req, res) => {
-  const { id } = req.params;
-  const { rows, rowCount } = await pool.query(
-    'DELETE FROM users WHERE id = $1 RETURNING *',
-    [id]
-  );
-  console.log(rows);
-
-  if (rowCount === 0) {
-    return res.status(400).json({ mesagge: 'User not found' });
-  }
-
-  return res.json(rows);
-});
+router.delete('/users/:id', deleteUser);
 
 //PUT
-router.put('/users/:id', (req, res) => {
-  const { id } = req.params;
-  res.send('actualizando usuarios' + id);
-});
+router.put('/users/:id', updateUser);
 
 export default router;
